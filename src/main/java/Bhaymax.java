217b6660d0fbcf4696a9ffceee3a1f366d5bd84e
HarishB99 <e1138766@u.nus.edu>
2025-01-23 10:26:36 +0800

    Bhaymax.java: Print greeting, farewell messages and exit

--- src/main/java/Bhaymax.java
+++ src/main/java/Bhaymax.java
@@ -1,10 +1,24 @@
 public class Bhaymax {
+    public static final String NAME = "Bhaymax";
+
+    public static void printHorizontalLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void greetHello(String name) {
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+    }
+
+    public static void sayFarewell() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Bhaymax.printHorizontalLine();
+        Bhaymax.greetHello(Bhaymax.NAME);
+        Bhaymax.printHorizontalLine();
+        Bhaymax.sayFarewell();
+        Bhaymax.printHorizontalLine();
     }
 }
