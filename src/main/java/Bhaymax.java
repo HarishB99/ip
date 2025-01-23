eda406e366af82c2032493af5ccdfc034d557150
HarishB99 <e1138766@u.nus.edu>
2025-01-23 10:54:44 +0800

    Bhaymax.java: Add indentation to output

--- src/main/java/Bhaymax.java
+++ src/main/java/Bhaymax.java
@@ -2,16 +2,16 @@ public class Bhaymax {
     public static final String NAME = "Bhaymax";
 
     public static void printHorizontalLine() {
-        System.out.println("____________________________________________________________");
+        System.out.println("\t____________________________________________________________");
     }
 
     public static void greetHello(String name) {
-        System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?");
+        System.out.println("\t Hello! I'm " + name);
+        System.out.println("\t What can I do for you?");
     }
 
     public static void sayFarewell() {
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("\t Bye. Hope to see you again soon!");
     }
 
     public static void main(String[] args) {
