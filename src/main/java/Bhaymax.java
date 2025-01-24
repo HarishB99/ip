c94d34a536ed9e79dae7fd3113471d168f20e7d5
HarishB99 <e1138766@u.nus.edu>
2025-01-24 13:11:28 +0800

    feat: Add ability to mark tasks as done/undone
    Task.java: Create Task class to store tasks
    Bhaymax.java: Implement commands to mark tasks as done/undone

--- src/main/java/Bhaymax.java
+++ src/main/java/Bhaymax.java
@@ -1,11 +1,12 @@
 import java.util.LinkedList;
-import java.util.ListIterator;
 import java.util.Scanner;
 
 public class Bhaymax {
     public static final String NAME = "Bhaymax";
-    public static final String LIST_COMMAND = "list";
-    public static final String EXIT_COMMAND = "bye";
+    public static final String COMMAND_LIST = "list";
+    public static final String COMMAND_MARK = "mark";
+    public static final String COMMAND_UNMARK = "unmark";
+    public static final String COMMAND_EXIT = "bye";
 
     public static void printHorizontalLine() {
         System.out.println("\t____________________________________________________________");
@@ -25,28 +26,80 @@ public class Bhaymax {
         Bhaymax.greetHello(Bhaymax.NAME);
         Bhaymax.printHorizontalLine();
 
-        LinkedList<String> tasks = new LinkedList<String>();
+        LinkedList<Task> tasks = new LinkedList<Task>();
         Scanner sc = new Scanner(System.in);
         String command = "";
 
-        while (!command.equals(Bhaymax.EXIT_COMMAND)) {
+        while (!command.equals(Bhaymax.COMMAND_EXIT)) {
             System.out.println();
             command = sc.nextLine();
-            if (command.equals(Bhaymax.EXIT_COMMAND)) {
+            if (command.equals(Bhaymax.COMMAND_EXIT)) {
                 break;
-            } else if (command.equals(Bhaymax.LIST_COMMAND)) {
+            } else if (command.equals(Bhaymax.COMMAND_LIST)) {
                 Bhaymax.printHorizontalLine();
                 if (tasks.isEmpty()) {
                     System.out.println("\t There are no tasks available.");
+                } else {
+                    System.out.println("\t Here are the tasks in your list:");
                 }
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println("\t " + (i + 1) + ". " + tasks.get(i));
+                    System.out.println("\t " + (i + 1) + "." + tasks.get(i));
+                }
+                Bhaymax.printHorizontalLine();
+                continue;
+            } else if (command.startsWith(Bhaymax.COMMAND_MARK)) {
+                Bhaymax.printHorizontalLine();
+                String[] tokens = command.split(" ");
+                if (tokens.length != 2) {
+                    System.out.println("\t Invalid command syntax provided. Try again.");
+                    Bhaymax.printHorizontalLine();
+                    continue;
+                }
+                try {
+                    int indexOfTaskToMark = Integer.parseInt(tokens[1]) - 1;
+                    if (indexOfTaskToMark < 0 || indexOfTaskToMark >= tasks.size()) {
+                        System.out.println("\t Provided Task Number could not be found.");
+                        Bhaymax.printHorizontalLine();
+                        continue;
+                    }
+                    Task taskToMark = tasks.get(indexOfTaskToMark);
+                    taskToMark.markAsDone();
+                    tasks.set(indexOfTaskToMark, taskToMark);
+                    System.out.println("\t Nice! I've marked this task as done:");
+                    System.out.println("\t   " + tasks.get(indexOfTaskToMark));
+                } catch (NumberFormatException e) {
+                    System.out.println("\t Invalid command syntax provided. Try again.");
+                }
+                Bhaymax.printHorizontalLine();
+                continue;
+            } else if (command.startsWith(Bhaymax.COMMAND_UNMARK)) {
+                Bhaymax.printHorizontalLine();
+                String[] tokens = command.split(" ");
+                if (tokens.length != 2) {
+                    System.out.println("\t Invalid command syntax provided. Try again.");
+                    Bhaymax.printHorizontalLine();
+                    continue;
+                }
+                try {
+                    int indexOfTaskToMark = Integer.parseInt(tokens[1]) - 1;
+                    if (indexOfTaskToMark < 0 || indexOfTaskToMark >= tasks.size()) {
+                        System.out.println("\t Provided Task Number could not be found.");
+                        Bhaymax.printHorizontalLine();
+                        continue;
+                    }
+                    Task taskToMark = tasks.get(indexOfTaskToMark);
+                    taskToMark.markAsUndone();
+                    tasks.set(indexOfTaskToMark, taskToMark);
+                    System.out.println("\t OK, I've marked this task as not done yet:");
+                    System.out.println("\t   " + tasks.get(indexOfTaskToMark));
+                } catch (NumberFormatException e) {
+                    System.out.println("\t Invalid command syntax provided. Try again.");
                 }
                 Bhaymax.printHorizontalLine();
                 continue;
             }
             Bhaymax.printHorizontalLine();
-            tasks.add(command);
+            tasks.add(new Task(command));
             System.out.println("\t added: " + command);
             Bhaymax.printHorizontalLine();
         }
