782d09fb54e496e03962306bd13a66ce962a7450
HarishB99 <e1138766@u.nus.edu>
2025-01-25 18:57:10 +0800

    Add test cases for blank input and erraneous inputs

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -11,6 +11,14 @@ if [ -e "./ACTUAL.TXT" ]
 then
     rm ACTUAL.TXT
 fi
+if [ -e "./ACTUAL_BLANK.TXT" ]
+then
+    rm ACTUAL_BLANK.TXT
+fi
+if [ -e "./ACTUAL_ERRANEOUS.TXT" ]
+then
+    rm ACTUAL_ERRANEOUS.TXT
+fi
 
 # compile the code into the bin folder, terminates if error occurred
 if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
@@ -21,10 +29,16 @@ fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 java -classpath ../bin Bhaymax < input.txt > ACTUAL.TXT
+java -classpath ../bin Bhaymax < input_blank.txt > ACTUAL_BLANK.TXT
+java -classpath ../bin Bhaymax < input_erraneous.txt > ACTUAL_ERRANEOUS.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
+cp EXPECTED_BLANK.TXT EXPECTED_BLANK-UNIX.TXT
+cp EXPECTED_ERRANEOUS.TXT EXPECTED_ERRANEOUS-UNIX.TXT
 dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
+dos2unix ACTUAL_BLANK.TXT EXPECTED_BLANK-UNIX.TXT
+dos2unix ACTUAL_ERRANEOUS.TXT EXPECTED_ERRANEOUS-UNIX.TXT
 
 # compare the output to the expected output
 diff ACTUAL.TXT EXPECTED-UNIX.TXT
@@ -34,5 +48,23 @@ then
     exit 0
 else
     echo "Test result: FAILED"
+    exit 1
+fi
+diff ACTUAL_BLANK.TXT EXPECTED_BLANK-UNIX.TXT
+if [ $? -eq 0 ]
+then
+    echo "Test result (blank input): PASSED"
+    exit 0
+else
+    echo "Test result (blank input): FAILED"
+    exit 1
+fi
+diff ACTUAL_ERRANEOUS.TXT EXPECTED_ERRANEOUS-UNIX.TXT
+if [ $? -eq 0 ]
+then
+    echo "Test result (erraneous input): PASSED"
+    exit 0
+else
+    echo "Test result (erraneous input): FAILED"
     exit 1
 fi
