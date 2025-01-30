fd54ca36cc04f47e6865e37db65b6dccf4d191ab
HarishB99 <e1138766@u.nus.edu>
2025-01-30 17:03:07 +0800

    Implement read file

--- src/main/java/Bhaymax.java
+++ src/main/java/Bhaymax.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.StringTokenizer;
@@ -15,6 +17,88 @@ public class Bhaymax {
     public static final String DEADLINE_OPT_BY  = "/by";
     public static final String EVENT_OPT_START  = "/from";
     public static final String EVENT_OPT_END    = "/to";
+    public static final String TASKS_FILE_PATH  = "data/tasks.txt";
+
+    public static boolean readTasksFromFile(String filePath, LinkedList<Task> tasks)
+            throws InvalidFileFormatException {
+        try {
+            File file = new File(filePath);
+            Scanner sc = new Scanner(file);
+            int lineNumber = 1;
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine().strip();
+                StringTokenizer tokenizer = new StringTokenizer(line, "|");
+                if (!tokenizer.hasMoreTokens()) {
+                    // File exists but is empty. Should be acceptable.
+                    return false;
+                }
+                String taskType = tokenizer.nextToken().strip();
+                if (!tokenizer.hasMoreTokens()) {
+                    throw new InvalidFileFormatException("Line " + lineNumber + ": Status of task is missing");
+                }
+                String taskStatus = tokenizer.nextToken().strip();
+                boolean taskWasDone;
+                try {
+                    taskWasDone = Integer.parseInt(taskStatus) == 1;
+                } catch (NumberFormatException e) {
+                    throw new InvalidFileFormatException(
+                            "Line " + lineNumber + ": Status of task is not a valid value");
+                }
+                if (!tokenizer.hasMoreTokens()) {
+                    throw new InvalidFileFormatException("Line " + lineNumber + ": Task description is missing");
+                }
+                String taskDescription = tokenizer.nextToken().strip();
+                Task task;
+                switch (taskType) {
+                case "T":
+                    task = new Todo(taskDescription);
+                    if (taskWasDone) {
+                        task.markAsDone();
+                    }
+                    break;
+                case "D":
+                    if (!tokenizer.hasMoreTokens()) {
+                        throw new InvalidFileFormatException(
+                                "Line " + lineNumber + ": Deadline Task doesn't have a deadline");
+                    }
+                    String deadline = tokenizer.nextToken().strip();
+                    task = new Deadline(taskDescription, deadline);
+                    if (taskWasDone) {
+                        task.markAsDone();
+                    }
+                    break;
+                case "E":
+                    if (!tokenizer.hasMoreTokens()) {
+                        throw new InvalidFileFormatException(
+                                "Line " + lineNumber + ": Event doesn't have a start date/time");
+                    }
+                    String start = tokenizer.nextToken().strip();
+                    if (!tokenizer.hasMoreTokens()) {
+                        throw new InvalidFileFormatException(
+                                "Line " + lineNumber + ": Event doesn't have an end date/time");
+                    }
+                    String end = tokenizer.nextToken().strip();
+                    task = new Event(taskDescription, start, end);
+                    if (taskWasDone) {
+                        task.markAsDone();
+                    }
+                    break;
+                default:
+                    throw new InvalidFileFormatException("Line " + lineNumber + ": Unrecognised task type");
+                }
+                tasks.add(task);
+                lineNumber++;
+            }
+            sc.close();
+            return true;
+        } catch (FileNotFoundException ignored) {
+            return false;
+        }
+    }
+
+    public static boolean writeTasksToFile(String filePath, LinkedList<Task> tasks) {
+        return false;
+    }
 
     public static void printWithIndent(String msg, boolean includeAdditionalSpace) {
         System.out.print("    ");
@@ -45,6 +129,21 @@ public class Bhaymax {
         Bhaymax.printHorizontalLine();
 
         LinkedList<Task> tasks = new LinkedList<Task>();
+        try {
+            boolean wasAbleToReadFile = Bhaymax.readTasksFromFile(Bhaymax.TASKS_FILE_PATH, tasks);
+            if (wasAbleToReadFile) {
+                Bhaymax.printHorizontalLine();
+                Bhaymax.printWithIndent("Task file was read successfully", true);
+                Bhaymax.printHorizontalLine();
+            }
+        } catch (InvalidFileFormatException e) {
+            System.out.println();
+            Bhaymax.printHorizontalLine();
+            Bhaymax.printWithIndent("[-] Format of task file is incorrect:", true);
+            Bhaymax.printWithIndent("    " + e.getMessage(), true);
+            Bhaymax.printWithIndent("[-] Please check your task file and try again", true);
+            Bhaymax.printHorizontalLine();
+        }
         Scanner sc = new Scanner(System.in);
         String userInput = "";
 
