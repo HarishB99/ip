b3375b0e43379bf57ce049062d0751f0e03116d4
HarishB99 <e1138766@u.nus.edu>
2025-02-02 22:21:38 +0800

    Add javadoc comments for stubs created

--- src/test/java/bhaymax/parser/TaskListStub.java
+++ src/test/java/bhaymax/parser/TaskListStub.java
@@ -2,5 +2,9 @@ package bhaymax.parser;
 
 import bhaymax.task.TaskList;
 
+/**
+ * A stub for {@link ParserTest}
+ * which relies on {@link TaskList}
+ */
 public class TaskListStub extends TaskList {
 }
--- src/test/java/bhaymax/task/TaskStub.java
+++ src/test/java/bhaymax/task/TaskStub.java
@@ -1,5 +1,9 @@
 package bhaymax.task;
 
+/**
+ * A stub for {@link TaskListTest}
+ * which relies on {@link Task}
+ */
 public class TaskStub extends Task {
     public TaskStub() {
         super("T", "Read book");
