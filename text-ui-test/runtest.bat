98aab02b42e4709e00e7ace0e9ef90bb175bf819
HarishB99 <e1138766@u.nus.edu>
2025-02-06 12:03:18 +0800

    runtest.bat: fix: Include package name when running with java

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -10,7 +10,7 @@ if exist ACTUAL_ERRONEOUS.TXT del ACTUAL_ERRONEOUS.TXT
 if exist data rmdir /s /q data
 
 REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\bhaymax\Bhaymax.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
@@ -18,22 +18,22 @@ IF ERRORLEVEL 1 (
 REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin Bhaymax < input.txt > ACTUAL.TXT
+java -classpath ..\bin bhaymax.Bhaymax < input.txt > ACTUAL.TXT
 rmdir /s /q data
-java -classpath ..\bin Bhaymax < input_blank.txt > ACTUAL_BLANK.TXT
+java -classpath ..\bin bhaymax.Bhaymax < input_blank.txt > ACTUAL_BLANK.TXT
 rmdir /s /q data
-java -classpath ..\bin Bhaymax < input_erroneous.txt > ACTUAL_ERRONEOUS.TXT
+java -classpath ..\bin bhaymax.Bhaymax < input_erroneous.txt > ACTUAL_ERRONEOUS.TXT
 
 REM compare the output to the expected output
 ECHO ========================================
 ECHO             NORMAL TEST
 ECHO ========================================
+FC ACTUAL.TXT EXPECTED.TXT
 ECHO ========================================
 ECHO             BLANK TEST
 ECHO ========================================
+FC ACTUAL_BLANK.TXT EXPECTED_BLANK.TXT
 ECHO ========================================
 ECHO             ERRONEOUS TEST
 ECHO ========================================
-FC ACTUAL.TXT EXPECTED.TXT
-FC ACTUAL_BLANK.TXT EXPECTED_BLANK.TXT
 FC ACTUAL_ERRONEOUS.TXT EXPECTED_ERRONEOUS.TXT
