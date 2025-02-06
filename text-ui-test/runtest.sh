738ea30123471669f48525b013db055a7f795d8e
HarishB99 <e1138766@u.nus.edu>
2025-02-06 15:20:19 +0800

    runtest.sh: Build Bhaymax.java instead of *.java

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -25,7 +25,7 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/bhaymax/Bhaymax.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
