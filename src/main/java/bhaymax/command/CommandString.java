3a1102331a8d9d603378dca1f0956b0f1779d844
HarishB99 <e1138766@u.nus.edu>
2025-02-06 12:00:17 +0800

    Move command string constants to its own Enum

---
+++ src/main/java/bhaymax/command/CommandString.java
@@ -0,0 +1,61 @@
+package bhaymax.command;
+
+import java.util.HashMap;
+
+import bhaymax.exception.InvalidCommandException;
+
+/**
+ * Provides enumeration values representing
+ * the valid commands a user is allowed to
+ * enter
+ */
+public enum CommandString {
+    LIST("list"),
+    MARK("mark"),
+    UNMARK("unmark"),
+    TODO("todo"),
+    DEADLINE("deadline"),
+    SEARCH("search"),
+    FILTER("filter"),
+    EVENT("event"),
+    DELETE("delete"),
+    BYE("bye"),
+    EXIT("exit");
+
+    private static final HashMap<String, CommandString> COMMAND_STRINGS = new HashMap<String, CommandString>();
+
+    static {
+        for (CommandString commandStringEnum : values()) {
+            COMMAND_STRINGS.put(
+                    commandStringEnum.command, commandStringEnum);
+        }
+    }
+
+    private final String command;
+
+    CommandString(String command) {
+        this.command = command;
+    }
+
+    public static boolean isValidCommandString(String commandString) {
+        return COMMAND_STRINGS.containsKey(commandString);
+    }
+
+    /**
+     * Returns the {@link CommandString} value corresponding to
+     * the command string entered by the user
+     *
+     * @param commandString the command string entered by the user
+     * @return a {@link CommandString} value corresponding to the
+     *         given command string, if the command string is a recognised one
+     * @throws InvalidCommandException If the command string provided is not recognised
+     */
+    public static CommandString valueOfCommandString(String commandString) throws InvalidCommandException {
+        for (CommandString commandStringEnum : values()) {
+            if (commandStringEnum.command.equals(commandString)) {
+                return commandStringEnum;
+            }
+        }
+        throw new InvalidCommandException();
+    }
+}
