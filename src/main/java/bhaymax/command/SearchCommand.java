1cfc94de849c7bab3bd9d0a9bff13d92d466ad54
HarishB99 <e1138766@u.nus.edu>
2025-02-15 00:27:46 +0800

    SearchCommand.java: Remove ununsed attributes

--- src/main/java/bhaymax/command/SearchCommand.java
+++ src/main/java/bhaymax/command/SearchCommand.java
@@ -11,8 +11,6 @@ import bhaymax.task.TaskList;
  */
 public class SearchCommand extends Command {
     private final String searchTerm;
-    private final String dateTime;
-    private final FilterOption filterOption;
 
     /**
      * Sets up the search term that will be used to filter a list of tasks
@@ -21,23 +19,6 @@ public class SearchCommand extends Command {
      */
     public SearchCommand(String searchTerm) {
         this.searchTerm = searchTerm;
-        this.dateTime = "";
-        this.filterOption = FilterOption.DATE_ON;
-    }
-
-    /**
-     * Sets up parameters used in search
-     *
-     * @param searchTerm the search term as a {@code String}
-     * @param dateTime the date and/or time to filter by, as a {@code String}
-     * @param filterOption a {@link FilterOption} enum value indicating the type of
-     *                  filter (i.e., before the date, after the date, exactly on
-     *                  the date, include/exclude time)
-     */
-    public SearchCommand(String searchTerm, String dateTime, FilterOption filterOption) {
-        this.searchTerm = searchTerm;
-        this.dateTime = dateTime;
-        this.filterOption = filterOption;
     }
 
     @Override
