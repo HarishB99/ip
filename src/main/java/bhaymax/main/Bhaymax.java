33195774ac937c8f727c6d9432fa76f604cad547
HarishB99 <e1138766@u.nus.edu>
2025-02-16 22:28:44 +0800

    Cleanup code and view for MainWindow

--- src/main/java/bhaymax/main/Bhaymax.java
+++ src/main/java/bhaymax/main/Bhaymax.java
@@ -6,82 +6,64 @@ import bhaymax.controller.MainWindow;
 import bhaymax.exception.file.InvalidFileFormatException;
 import bhaymax.storage.Storage;
 import bhaymax.task.TaskList;
-import javafx.animation.PauseTransition;
+import bhaymax.util.Pair;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 /**
- * Main class for JavaFX version of the Chatbot app
+ * Loads the main window for the chatbot
  */
 public class Bhaymax extends Application {
     public static final String APP_NAME = "Bhaymax";
 
     private static final int PREFERRED_WIDTH = 420;
     private static final int PREFERRED_HEIGHT = 640;
-    // Specified in milliseconds
-    private static final int PAUSE_DURATION_FOR_TERMINATION = 5000;
-    private static final String ERROR_MESSAGE_TERMINATING_APP = "I will terminate in 5 seconds";
 
-    private final Storage storage;
-    private TaskList tasks;
+    private static final String ERROR_MESSAGE_RECTIFY_FILE_ERRORS =
+            "Please close this app, rectify the errors in file, and try again.";
 
-    /**
-     * Sets up the UI and loads the task
-     * from a file, if it exists
-     */
-    public Bhaymax() {
-        this.storage = new Storage();
-    }
-
-    @Override
-    public void start(Stage stage) {
-        AnchorPane anchorPane;
-        MainWindow mainWindowController;
+    private Pair<MainWindow, AnchorPane> loadMainWindowAndAnchorPane() {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(
                     Bhaymax.class.getResource(FxmlFilePath.MAIN_WINDOW.toString()));
-            anchorPane = fxmlLoader.load();
-            mainWindowController = fxmlLoader.<MainWindow>getController();
+            AnchorPane anchorPane = fxmlLoader.load();
+            MainWindow mainWindowController = fxmlLoader.<MainWindow>getController();
+            return new Pair<MainWindow, AnchorPane>(mainWindowController, anchorPane);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
 
-        boolean canOpenFile = false;
-        Scene scene = new Scene(anchorPane);
-        stage.setScene(scene);
-
-        // Set up properties for MainWindow controller
-        mainWindowController.setStorage(this.storage);
-
-        PauseTransition pauseTransition = new PauseTransition(
-                Duration.millis(Bhaymax.PAUSE_DURATION_FOR_TERMINATION));
-        pauseTransition.setOnFinished(event -> Platform.exit());
-        try {
-            this.tasks = this.storage.loadTasks();
-            canOpenFile = true;
-        } catch (InvalidFileFormatException e) {
-            mainWindowController.disableInputs();
-            mainWindowController.clearChat(false);
-            mainWindowController.showInvalidFileFormatDialogBox(e);
-            mainWindowController.showSadResponse(Bhaymax.ERROR_MESSAGE_TERMINATING_APP);
-            pauseTransition.play();
-        }
-
-        if (canOpenFile) {
-            mainWindowController.showWelcomeDialogBox(Bhaymax.APP_NAME);
-            mainWindowController.setTasks(this.tasks);
-        }
-
+    private void prepareStage(Stage stage) {
         stage.setMinHeight(Bhaymax.PREFERRED_HEIGHT);
         stage.setMinWidth(Bhaymax.PREFERRED_WIDTH);
         stage.setMaxHeight(Bhaymax.PREFERRED_HEIGHT);
         stage.setMaxWidth(Bhaymax.PREFERRED_WIDTH);
+    }
 
+    @Override
+    public void start(Stage stage) {
+        Pair<MainWindow, AnchorPane> controllerAndPane = this.loadMainWindowAndAnchorPane();
+        MainWindow mainWindowController = controllerAndPane.first();
+        AnchorPane anchorPane = controllerAndPane.second();
+
+        try {
+            Storage storage = new Storage();
+            TaskList tasks = storage.loadTasks();
+            mainWindowController.setTasks(tasks);
+            mainWindowController.setStorage(storage);
+            mainWindowController.showWelcomeDialogBox(Bhaymax.APP_NAME);
+        } catch (InvalidFileFormatException e) {
+            mainWindowController.disableInputs();
+            mainWindowController.showInvalidFileFormatDialogBox(e);
+            mainWindowController.showSadResponse(Bhaymax.ERROR_MESSAGE_RECTIFY_FILE_ERRORS);
+        }
+
+        this.prepareStage(stage);
+        stage.setScene(new Scene(anchorPane));
         stage.show();
     }
 }
