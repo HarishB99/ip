e80e9671be95a05f5c86197287ef618add1411ad
HarishB99 <e1138766@u.nus.edu>
2025-02-20 20:47:55 +0800

    Include command format in error message when missing search term

--- src/main/java/bhaymax/exception/command/MissingSearchTermException.java
+++ src/main/java/bhaymax/exception/command/MissingSearchTermException.java
@@ -4,7 +4,8 @@ package bhaymax.exception.command;
  * Thrown when no search term is provided for the {@link bhaymax.command.SearchCommand}
  */
 public class MissingSearchTermException extends InvalidCommandFormatException {
-    public static final String ERROR_MESSAGE = "A search term (or phrase) is required to perform the search operation.";
+    public static final String ERROR_MESSAGE = "The format of a search command is 'search <search term or phrase>'. "
+            + "You are missing: <search term or phrase>";
 
     public MissingSearchTermException() {
         super(MissingSearchTermException.ERROR_MESSAGE);
